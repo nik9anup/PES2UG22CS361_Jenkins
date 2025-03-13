ccd81364eabf2ae939d3c0a771d3c808839f05e7
nik9anup <nik.anup@outlook.com>
2025-03-13 15:29:24 +0530

    changed hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main(void) {
+  cout << "PES2UG22CS361 Nikita Anup\n";
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
